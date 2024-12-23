7e12d4fa48f0ba29023835c1c2009be54cde0479
Ryan Cabell <cabell.ryan@gmail.com>
2024-12-23 17:18:10 -0500

    Added unversioned files

--- app/src/main/java/com/example/myapplication/ui/theme/Color.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication.ui.theme
+package com.example.shelvz.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
--- app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/myapplication/ExampleUnitTest.kt
@@ -1,4 +1,4 @@
-package com.example.myapplication
+package com.example.shelvz
 
 import org.junit.Test
 
