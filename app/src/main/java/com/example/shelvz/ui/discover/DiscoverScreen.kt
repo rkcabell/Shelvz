e9a1bb799b4f5de7190c3763aa9fed9016af22e0
Ryan Cabell <cabell.ryan@gmail.com>
2025-01-01 14:06:30 -0500

    MVVM Architecture files

--- app/src/main/java/com/example/shelvz/ui/discover/DiscoverScreen.kt
+++ app/src/main/java/com/example/shelvz/ui/discover/DiscoverScreen.kt
@@ -8,13 +8,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DiscoverScreen(navController: NavController) {
     Scaffold(
-        topBar = { TopAppBar(title = { Text("Discover") }) }
+        topBar = { TopAppBar(title = { Text("Discover", fontWeight = FontWeight.Bold, fontSize = 40.sp) }) }
     ) { paddingValues ->
         Button(
             onClick = { navController.navigate("destination_route") },
